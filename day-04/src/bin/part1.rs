0b52a977d9c480fcd1f39c9e4ecb41bcd424da39
George-Anagnostou <anagnostougeorgejohn@gmail.com>
2023-12-17 20:38:12 -0800

    finish day-04 part1

---
+++ day-04/src/bin/part1.rs
@@ -0,0 +1,80 @@
+fn main() {
+    let input = include_str!("../../input.txt");
+    let total_points = calculate_total_points(&input);
+    println!("{}", total_points);
+}
+
+fn calculate_total_points(input: &str) -> usize {
+    input
+        .lines()
+        .map(parse_line)
+        .map(get_line_total)
+        .sum()
+}
+
+fn parse_line(line: &str) -> (Vec<usize>, Vec<usize>) {
+    let (winners, ours) = line
+        .split(":")
+        .last()
+        .unwrap()
+        .split_once("|")
+        .unwrap();
+
+    let winners_vec: Vec<usize> = winners
+        .split_whitespace()
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+    let ours_vec: Vec<usize> = ours
+        .split_whitespace()
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+    (winners_vec, ours_vec)
+}
+
+fn get_line_total(line_pair: (Vec<usize>, Vec<usize>)) -> usize {
+    let (winners_vec, ours_vec) = line_pair;
+    let mut count = 0;
+
+    for winner in &winners_vec {
+        if ours_vec.contains(winner) {
+            count += 1;
+        }
+    }
+
+    score_numbers(count)
+}
+
+fn score_numbers(n: usize) -> usize {
+    if n == 0 || n == 1 { return n };
+    let mut k = 1;
+    for _ in 1..n {
+        k *= 2;
+    }
+    k
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_parse_line() {
+        assert_eq!(parse_line("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"),
+                   (vec![41, 48, 83, 86, 17], vec![83, 86, 6, 31, 17, 9, 48, 53]));
+        assert_eq!(parse_line("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"),
+                   (vec![13, 32, 20, 16, 61], vec![61, 30, 68, 82, 17, 32, 24, 19]));
+    }
+
+    #[test]
+    fn test_get_line_total() {
+        assert_eq!(get_line_total((vec![41, 48, 83, 86, 17], vec![83, 86, 6, 31, 17, 9, 48, 53])), 8);
+        assert_eq!(get_line_total((vec![13, 32, 20, 16, 61], vec![61, 30, 68, 82, 17, 32, 24, 19])), 2);
+    }
+
+    #[test]
+    fn test_score_numbers() {
+        assert_eq!(score_numbers(4), 8);
+        assert_eq!(score_numbers(2), 2);
+        assert_eq!(score_numbers(1), 1);
+        assert_eq!(score_numbers(0), 0);
+    }
+}
