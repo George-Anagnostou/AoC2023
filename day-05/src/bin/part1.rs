0afbdc8fff68b23c7eca7f549e9f77c3de354041
George-Anagnostou <anagnostougeorgejohn@gmail.com>
2023-12-27 22:03:11 -0800

    finish day-05 part1

---
+++ day-05/src/bin/part1.rs
@@ -0,0 +1,99 @@
+fn main() {
+    let input = include_str!("../../input.txt");
+    let lowest_location = find_lowest_location(input);
+    println!("{}", lowest_location);
+}
+
+#[derive(Debug)]
+struct RangeMap {
+    destination_range: usize,
+    source_range: usize,
+    range_length: usize,
+}
+
+impl RangeMap {
+    fn from(line: &str) -> Self {
+        let nums: Vec<usize> = line
+            .split_whitespace()
+            .map(|num| num.parse::<usize>().unwrap())
+            .collect();
+
+        Self {
+            destination_range: nums[0],
+            source_range: nums[1],
+            range_length: nums[2],
+        }
+    }
+}
+
+fn get_location(seed: usize, mappings: &Vec<Vec<RangeMap>>) -> usize {
+    let mut num: usize = seed;
+    for section in mappings {
+        for row in section {
+            if (row.source_range..row.source_range + row.range_length).contains(&num) {
+                let diff = num - row.source_range;
+                num = row.destination_range + diff;
+                break;
+
+            }
+        }
+    }
+    num
+}
+
+fn get_almanac(parsed_input: Vec<Vec<&str>>) -> Vec<Vec<RangeMap>> {
+    parsed_input
+        .into_iter()
+        .map(|section| section.into_iter()
+             .filter(|line| !line.is_empty())
+             .map(RangeMap::from)
+             .collect())
+        .collect()
+}
+
+fn get_seeds(parsed_input: &mut Vec<Vec<&str>>) -> Vec<usize> {
+    parsed_input
+        .remove(0)
+        .first()
+        .unwrap()
+        .split_whitespace()
+        .map(|num| num.parse::<usize>().unwrap())
+        .collect()
+}
+
+fn parse_input(input: &str) -> Vec<Vec<&str>> {
+    input
+        .split("\n\n")
+        .map(|mapping|
+             mapping
+             .split(":")
+             .last()
+             .unwrap()
+             .lines()
+             .collect()
+        )
+        .collect()
+}
+
+fn find_lowest_location(input: &str) -> usize {
+    let mut parsed_input = parse_input(input);
+    let seeds = get_seeds(&mut parsed_input);
+    let mappings = get_almanac(parsed_input);
+
+    seeds
+        .into_iter()
+        .map(|seed| get_location(seed, &mappings))
+        .min()
+        .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_lowest_location() {
+        let input = include_str!("../../small_input.txt");
+        assert_eq!(find_lowest_location(input), 35);
+    }
+}
